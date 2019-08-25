af78aaac8982561812508b7653ff40c842bba8b7
Javone Smith <javosmith12@gmail.com>
2019-08-25 14:33:46 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,13 @@
 # code your #valid_move? method here
+# obj of method is to check if move is valid
+
+def valid_move?(board, index)
+  if board[index] == nil
+    return false
+  elsif !position_taken?(board,index)
+    return true
+  end
+end 
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
